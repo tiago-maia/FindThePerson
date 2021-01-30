fc4effdefd3d67e22fc9f16e58b5d96d593de0b6
Daniel Lobo <feloxolofoxifalitu@gmail.com>
2021-01-30 12:21:46 +0000

    refactored person assets; fixed bug setting materials; minor changes;

---
+++ FindTheRetard/Assets/Scripts/Assets.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class Assets : ScriptableObject
+{
+	[Serializable]
+	public class MaterialAndSpritePair
+	{
+		public Material Material;
+		public Sprite UIImage;
+	}
+
+	[Serializable]
+	public class MeshAndSpritePair
+	{
+		public Mesh Mesh;
+		public Sprite UIImage;
+	}
+
+	public MeshAndSpritePair[] HeadAccessories;
+	public MaterialAndSpritePair[] MaskMaterials;
+	public MaterialAndSpritePair[] ShirtMaterials;
+	public MaterialAndSpritePair[] PantsMaterials;
+}
--- FindTheRetard/Assets/Scripts/GameManager.cs
+++ FindTheRetard/Assets/Scripts/GameManager.cs
@@ -1,35 +1,40 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class GameManager : MonoBehaviour
 {
 	// Need to randomly create a number of persons ( range ? 20 - 30 ? Increase with difficulty ? ) and to randomize their clothes
 	// Create instance of Correct Person
 
+	[SerializeField] Assets assets;
 	[SerializeField] Transform peopleParent;
-
-	[SerializeField] Mesh[] headAccessories;
-	[SerializeField] Material[] maskMaterials;
-	[SerializeField] Material[] shirtMaterials;
-	[SerializeField] Material[] pantsMaterials;
 
 	[SerializeField] Person personPrefab;
 
 	List<Person> people = new List<Person>();
 
+	readonly Vector3 mapSize = new Vector3(24, 0, 24);
+	Person targetPerson;
+
 	void Start()
 	{
-		CreateNewGame(50);
+		CreateNewGame(1000);
 	}
 
 	void CreateNewGame(int nPeople)
 	{
-		for (int i = 0; i < nPeople; i++)
+		this.targetPerson = InstantiateRandomPerson();
+		this.targetPerson.Setup(GetRandomPersonAssets(), mapSize);
+		people.Add(this.targetPerson);
+
+		for (int i = 0; i < nPeople-1; i++)	// -1 since target was already chosen
 		{
 			Person person = InstantiateRandomPerson();
+			person.Setup(GetRandomPersonAssetsDifferentFrom(targetPerson.PersonAssets), mapSize);
 			people.Add(person);
-			RandomizePerson(person);
 		}
 	}
 
@@ -39,18 +44,39 @@ public class GameManager : MonoBehaviour
 		return person;
 	}
 
-	void RandomizePerson(Person person)
+	PersonAssets GetRandomPersonAssets()
 	{
-		Mesh headAccessory = ChooseRandom(headAccessories);
-		Material maskMaterial = ChooseRandom(maskMaterials);
-		Material shirtMaterial = ChooseRandom(shirtMaterials);
-		Material pantsMaterial = ChooseRandom(pantsMaterials);
+		return new PersonAssets {
+			// HeadAccessory = ChooseRandom(assets.HeadAccessories),
+			MaskMaterial = ChooseRandom(assets.MaskMaterials),
+			ShirtMaterial = ChooseRandom(assets.ShirtMaterials),
+			PantsMaterial = ChooseRandom(assets.PantsMaterials)
+		};
+	}
 
-		person.Setup(maskMaterial, shirtMaterial, pantsMaterial, headAccessory);
+	PersonAssets GetRandomPersonAssetsDifferentFrom(PersonAssets personAssets)
+	{
+		return new PersonAssets {
+			// HeadAccessory = ChooseRandomButDifferent(assets.HeadAccessories, personAssets.HeadAccessory),
+			MaskMaterial = ChooseRandomButDifferent(assets.MaskMaterials, personAssets.MaskMaterial),
+			ShirtMaterial = ChooseRandomButDifferent(assets.ShirtMaterials, personAssets.ShirtMaterial),
+			PantsMaterial = ChooseRandomButDifferent(assets.PantsMaterials, personAssets.PantsMaterial)
+		};
 	}
 
 	T ChooseRandom<T>(T[] array)
 	{
 		return array[Random.Range(0, array.Length)];
+	}
+
+	T ChooseRandomButDifferent<T>(T[] array, T exclusion) where T : class
+	{
+		T choice;
+
+		do {
+			choice = array[Random.Range(0, array.Length)];
+		} while (choice == exclusion);
+
+		return choice;
 	}
 }
