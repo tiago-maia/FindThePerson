d0792b97084197fe1941d8c52950da59ad746021
Maia <jtdmaia@gmail.com>
2021-01-30 00:08:23 +0000

    Beggin The VOYAGE

---
+++ FindTheRetard/Assets/Scripts/GameManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	// Need to randomly create a number of persons ( range ? 20 - 30 ? Increase with difficulty ? ) and to randomize their clothes
+	// Create instance of Correct Person
+
+	[SerializeField] Transform peopleParent;
+	
+	[SerializeField] Mesh[] headAccessories;
+	[SerializeField] Material[] maskMaterials;
+	[SerializeField] Material[] shirtMaterials;
+	[SerializeField] Material[] pantsMaterials;
+	
+	[SerializeField] Person personPrefab;
+
+	List<Person> people = new List<Person>();
+
+	void Start()
+	{
+		CreateNewGame(3000);
+	}
+
+	void CreateNewGame(int nPeople)
+	{
+		for (int i = 0; i < nPeople; i++)
+		{
+			Person person = InstantiateRandomPerson();
+			people.Add(person);
+			RandomizePerson(person);
+		}
+	}
+
+	Person InstantiateRandomPerson()
+	{
+		Person person = Instantiate(personPrefab, peopleParent);
+		return person;
+	}
+
+	void RandomizePerson(Person person)
+	{
+		Mesh headAccessory = ChooseRandom(headAccessories);
+		Material maskMaterial = ChooseRandom(maskMaterials);
+		Material shirtMaterial = ChooseRandom(shirtMaterials);
+		Material pantsMaterial = ChooseRandom(pantsMaterials);
+
+		person.Setup(maskMaterial, shirtMaterial, pantsMaterial, headAccessory);
+	}
+
+	T ChooseRandom<T>(T[] array)
+	{
+		return array[Random.Range(0, array.Length)];
+	}
+}
---
+++ FindTheRetard/Assets/Scripts/Person.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Person : MonoBehaviour
+{
+	[SerializeField]
+	private MeshRenderer meshRenderer;
+	[SerializeField]
+	private MeshFilter headMeshFilter;
+	[SerializeField]
+	private MeshRenderer headMeshRenderer;
+
+	private Material maskMaterial;
+	private Material shirtMaterial;
+	private Material pantsMaterial;
+
+	private Vector3 targetPoint;
+	private NavMeshAgent navMeshAgent;
+    private const float TARGET_REACHED_DISTANCE = 1f;
+
+	void Start()
+	{
+		navMeshAgent = GetComponent<NavMeshAgent>();
+        SetRandomTarget();
+	}
+
+    void Update()
+    {
+        if ((targetPoint - transform.position).magnitude < TARGET_REACHED_DISTANCE) {
+				SetRandomTarget();
+			}
+    }
+
+	public void Setup(Material maskMaterial, Material shirtMaterial, Material pantsMaterial, Mesh headAccessory)
+	{
+		meshRenderer.materials[0] = maskMaterial;
+		meshRenderer.materials[1] = shirtMaterial;
+		meshRenderer.materials[2] = pantsMaterial;
+		headMeshFilter.mesh = headAccessory;
+	}
+
+	private void SetRandomTarget()
+	{
+		targetPoint = new Vector3(Random.Range(-30f, 30f), 0, Random.Range(-30f, 30f));
+		navMeshAgent.destination = targetPoint;
+	}
+}
