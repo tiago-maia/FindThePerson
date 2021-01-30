52b088b235e2d7659919ca0414b83809ad8441d8
Daniel Lobo <feloxolofoxifalitu@gmail.com>
2021-01-30 20:39:07 +0000

    implemented forced aspect ratio stuff

---
+++ FindTheRetard/Assets/Scripts/CameraAspectRatioAdapter.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class CameraAspectRatioAdapter : MonoBehaviour
+{
+	new Camera camera;
+	float originalOrthographicSize;
+
+	void Awake()
+	{
+		camera = GetComponent<Camera>();
+		originalOrthographicSize = camera.orthographicSize;
+	}
+
+	void Update()
+	{
+		float aspectRatio = Screen.width / (float) Screen.height;
+		aspectRatio = Mathf.Min(aspectRatio, 16f/9f);
+		camera.orthographicSize = originalOrthographicSize / (aspectRatio / (16f/9f));
+	}
+}
